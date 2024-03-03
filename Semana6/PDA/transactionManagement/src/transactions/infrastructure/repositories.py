93a78f82cd1e93d2187ba0401564956f0b810c67
Nelson <n.ortizf@uniandes.edu.co>
2024-03-03 09:22:27 -0500

    adding implementation of infrastructure

---
+++ Semana6/PDA/transactionManagement/src/transactions/infrastructure/repositories.py
@@ -0,0 +1,27 @@
+from src.config.db import db
+from uuid import UUID
+
+from src.transactions.domain.repositories import TransactionRepository
+from src.transactions.domain.factories import TransactionFactory
+from src.transactions.domain.entities import Transaction
+from .mappers import TransactionMapper
+
+class TransactionPostgresRepository(TransactionRepository):
+
+    def __init__(self):
+        self._transaction_factory: TransactionFactory = (
+            TransactionFactory()
+        )
+
+    @property
+    def credential_factory(self):
+        return self._transaction_factory
+    
+    def get_by_id(self, id: UUID) -> Transaction:
+        raise NotImplementedError
+    
+    def add(self, transaction: Transaction):
+        transaction_dto = self._transaction_factory.create_object(
+            transaction, TransactionMapper()
+        )
+        db.session.add(transaction_dto)
