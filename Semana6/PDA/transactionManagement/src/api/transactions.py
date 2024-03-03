4eb93a3866bac64d40f6c40401cc8c2f551d3121
Nelson <n.ortizf@uniandes.edu.co>
2024-03-03 10:06:34 -0500

    update API for adding a transaction

---
+++ Semana6/PDA/transactionManagement/src/api/transactions.py
@@ -0,0 +1,29 @@
+from http import HTTPStatus
+from flask import request
+
+from src.seedwork.presentation.api import crear_blueprint
+from src.transactions.application.commands.save_transaction import SaveTransaction
+from src.transactions.application.mappers import MapperTransactionDTOJson
+from src.seedwork.application.commands import exec_command
+from src.seedwork.domain.exceptions import DomainException
+
+properties_bp = crear_blueprint('transactions', '/transactions')
+
+@properties_bp.route("add", methods=["POST"])
+def add_transaction():
+    try:
+        transaction_dict = request.json
+        transaction_map = MapperTransactionDTOJson()
+        py_dto = transaction_map.external_to_dto(transaction_dict)
+        command = SaveTransaction(dni_landlord=py_dto.dni_landlord,
+                               dni_tenant=py_dto.dni_tenant,
+                               monetary_value=py_dto.monetary_value,
+                               type_lease=py_dto.type_lease,
+                               contract_initial_date=py_dto.contract_initial_date,
+                               contract_final_date=py_dto.contract_final_date)
+        exec_command(command)
+        return { 'msg': 'Transaction saved'}, HTTPStatus.ACCEPTED
+    except DomainException as e:
+        return { 'msg': str(e)}, HTTPStatus.INTERNAL_SERVER_ERROR
+    except Exception as e:
+        return { 'msg': str(e)}, HTTPStatus.BAD_REQUEST
