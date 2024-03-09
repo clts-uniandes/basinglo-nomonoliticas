d0f9d39d1f08dc77dedf1cf46b58f9abd494743d
Cristian Leonardo Toro Sarmiento <c.toros@uniandes.edu.co>
2024-03-09 00:42:39 -0500

    version users sin pulsar pero con cloud sql, imagen reducida

--- Semana7/userManagement/src/modules/auth/application/__init__.py
+++ Semana7/userManagement/src/modules/auth/application/__init__.py
@@ -2,4 +2,4 @@ from pydispatch import dispatcher
 from .handlers import DomainCredentialHandler
 
 #dispatcher.connect(DomainCredentialHandler.handle_credential_created, signal='CredentialCreatedDomain')
-dispatcher.connect(DomainCredentialHandler.handle_credential_created, signal='CredentialCreatedIntegration')
+#dispatcher.connect(DomainCredentialHandler.handle_credential_created, signal='CredentialCreatedIntegration')
